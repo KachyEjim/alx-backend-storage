1ed8f67e84b678882fbec763823191566c4f4655
root <ejimovc@gmail.com>
2024-08-13 12:02:39 +0100

    Amending task 6 and 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginix logs stored in a mongodb"""
+""" Provides stats about Nginx logs stored in MongoDB """
+
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def print_log_stats():
+    """
+    Counts the number of data in a nginix log
+    """
     client = MongoClient("mongodb://127.0.0.1:27017")
 
     db = client.logs
     nginx_collection = db.nginx
-    total = nginx_collection.count_documents({})
-    get = nginx_collection.count_documents({"method": {"$eq": "GET"}})
-    post = nginx_collection.count_documents({"method": {"$eq": "POST"}})
-    put = nginx_collection.count_documents({"method": {"$eq": "PUT"}})
-    patch = nginx_collection.count_documents({"method": {"$eq": "PATCH"}})
-    delete = nginx_collection.count_documents({"method": {"$eq": "DELETE"}})
-    status = nginx_collection.count_documents({"method": "GET", "path": "/status"})
 
-    print(f"{total} logs")
-    print(
-        f"methods:\n\tmethod GET: {get}\n\tmethod POST: {post}\n\tmethod PUT: {put}\n\tmethod PATCH: {patch}\n\tmethod DELETE: {delete}\n{status} status check"
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
     )
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    print_log_stats()
